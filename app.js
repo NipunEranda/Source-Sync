9ef38c2a67a08dea1ce0a6de38909b2779199f75
NipunEranda <namarasekara71@gmail.com>
2022-05-26 21:16:52 +0530

    routes seperately defined in routes.js file

--- app.js
+++ app.js
@@ -6,14 +6,9 @@ const MySQLStore = require('express-mysql-session')(session);
 const flash = require('connect-flash');
 const config = require('./config');
 const utils = require('./js/utils');
-
-const axios = require('axios');
-
-const homepage = require('./js/routes/homepage');
+const routes = require('./js/routes/routes');
 
 const app = express();
-
-var access_token = "";
 
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -60,51 +55,34 @@ app.use((req, res, next) => {
 });
 
 //Routes
-app.get('/signin', (req, res) => {
-    res.render('signin', {
-        'layout': null, // no layout for signin page
-        'github_client_id': config.github.credentials.client,
-    })
+Object.keys(routes()).forEach(route => {
+    Object.keys(routes()[route]).forEach(r => {
+        app.get(r, routes()[route][r]);
+    });
 });
 
-app.get('/github/callback', (req, res) => {
+// Object.keys(routes.post()).forEach(route => {
+//     app.post(route, routes.post()[route]);
+// });
 
-    // The req.query object has the query params that were sent to this route.
-    const requestToken = req.query.code
-    console.log(requestToken);
+// Object.keys(routes.put()).forEach(route => {
+//     app.put(route, routes.put()[route]);
+// });
 
-    axios({
-        method: 'post',
-        url: `https://github.com/login/oauth/access_token?client_id=${config.github.credentials.client}&client_secret=${config.github.credentials.secret}&code=${requestToken}`,
-        // Set the content type header, so that we get the response in JSON
-        headers: {
-            accept: 'application/json'
-        }
-    }).then((response) => {
-        req.session.token = response.data.access_token
-        res.redirect('/');
-    })
-})
-
-app.get('/', homepage.get);
-app.get('/signout', (req, res) => signout(req, res));
+// Object.keys(routes.delete()).forEach(route => {
+//     app.delete(route, routes.delete()[route]);
+// });
 
 app.listen(config.port, () => {
     utils.log(`Server listening on port ${config.port}.`);
 });
 
-function signout(req, res) {
-    req.session.token = null;
-    res.redirect('/signin');
-}
-
+//Check if user loggedIn or not
 function ensureSignedIn(req, res, next) {
-    // skip the check for the actual /signin page, duh!
     const path = req.path.toString();
     if (path === '/signin' || path === '/github/callback' || path.includes('/tokens/verify') || path.includes('/api')) {
         return next();
     }
-    // if the username is set then this means they're signed in
     if (req.session.token) {
         return next();
     }
